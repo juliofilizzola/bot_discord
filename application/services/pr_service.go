0f06bc4645b160631378553584ba31f38007ef34
julio_filizzola <juliofilizzola@hotmail.com>
2024-10-13 17:11:34 -0300

    Add PRService implementation
    Introduces `PRService` with methods to create, get, and delete PRs. This service depends on `PRRepository` for data persistence and retrieval.

---
+++ application/services/pr_service.go
@@ -0,0 +1,30 @@
+package services
+
+import (
+	"github.com/juliofilizzola/bot_discord/application/domain/model"
+	"github.com/juliofilizzola/bot_discord/application/domain/repository"
+)
+
+type PRService struct {
+	repo repository.PRRepository
+}
+
+func NewPRService(repo repository.PRRepository) *PRService {
+	return &PRService{repo}
+}
+
+func (s *PRService) CreatePR(pr *model.PR) error {
+	return s.repo.Save(pr)
+}
+
+func (s *PRService) GetPRByID(id string) (*model.PR, error) {
+	return s.repo.FindByID(id)
+}
+
+func (s *PRService) GetAllPRs() ([]*model.PR, error) {
+	return s.repo.FindAll()
+}
+
+func (s *PRService) DeletePR(id string) error {
+	return s.repo.Delete(id)
+}
