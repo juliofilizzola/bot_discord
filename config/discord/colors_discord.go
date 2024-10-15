82ddb63537a29d7dd09e9654873d9ba6f3ae43c1
julio_filizzola <juliofilizzola@hotmail.com>
2024-10-15 10:47:22 -0300

    Add predefined color constants to Discord config
    Introduce constants for commonly used colors in the 'colors_discord.go' file to enhance code readability and maintainability. This addition simplifies the usage of colors within the Discord-related configuration by using predefined values.

---
+++ config/discord/colors_discord.go
@@ -0,0 +1,12 @@
+package discord
+
+const (
+	ColorRed    = 15548997
+	ColorGreen  = 5763719
+	ColorBlue   = 3447003
+	ColorYellow = 16776960
+	ColorPurple = 10181046
+	ColorOrange = 15105570
+	ColorWhite  = 16777215
+	ColorBlack  = 0
+)
