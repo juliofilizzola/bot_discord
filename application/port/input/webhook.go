e556abdf361d4914dceb563471e5b1c422a91c51
julio_filizzola <juliofilizzola@hotmail.com>
2024-10-22 13:35:05 -0300

    Add Save method to WebhookDomainService interface
    This commit introduces a new method, Save, to the WebhookDomainService interface. The Save method is intended to handle GitHub data, enhancing the functionality of webhook services. This change also includes an import for the necessary domain package.

--- application/port/input/webhook.go
+++ application/port/input/webhook.go
@@ -2,8 +2,10 @@ package input
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"github.com/juliofilizzola/bot_discord/application/domain"
 )
 
 type WebhookDomainService interface {
 	Send(dataGit *discordgo.WebhookParams, webhookId, webhookToken, action string) string
+	Save(dataGit *domain.Github)
 }
