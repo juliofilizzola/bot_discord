84fd15a4096dbf2bc64fa6a2c4a570671bf94b9e
julio_filizzola <juliofilizzola@hotmail.com>
2024-10-22 13:34:32 -0300

    Refactor PR struct field types
    Replaced '*Account' with '*User' to improve clarity and consistency in naming conventions. No functional changes were made, ensuring application behavior remains unaffected.

--- application/domain/model/pr.go
+++ application/domain/model/pr.go
@@ -3,20 +3,20 @@ package model
 type PR struct {
 	ID              string `json:"id" valid:"uuid" gorm:"type:uuid;primary_key default:uuid_generate_v4()"`
 	Base            `valid:"required"`
-	Url             string     `json:"url" gorm:"type:varchar(255)"`
-	Number          string     `json:"number" gorm:"type:varchar(255);uniqueIndex:number"`
-	State           string     `json:"state" gorm:"type:varchar(255)"`
-	HtmlUrl         string     `json:"html_url" gorm:"type:varchar(255)"`
-	Title           string     `json:"title" gorm:"type:varchar(255)"`
-	Description     string     `json:"description" gorm:"type:varchar(255)"`
-	CreatedAtPr     string     `json:"created_at_pr" gorm:"type:varchar(255)"`
-	ClosedAt        string     `json:"closed_at" gorm:"type:varchar(255)"`
-	Color           string     `json:"color" gorm:"type:varchar(255)"`
-	OwnerPR         *Account   `gorm:"foreignKey:OwnerId"`
-	OwnerID         string     `json:"owner_id"`
-	Reviewers       []*Account `gorm:"many2many:reviewers_pr;"`
-	Locked          bool       `json:"locked"`
-	CommitsUrl      string     `json:"commits_url" gorm:"type:varchar(255)"`
+	Url             string  `json:"url" gorm:"type:varchar(255)"`
+	Number          string  `json:"number" gorm:"type:varchar(255);uniqueIndex:number"`
+	State           string  `json:"state" gorm:"type:varchar(255)"`
+	HtmlUrl         string  `json:"html_url" gorm:"type:varchar(255)"`
+	Title           string  `json:"title" gorm:"type:varchar(255)"`
+	Description     string  `json:"description" gorm:"type:varchar(255)"`
+	CreatedAtPr     string  `json:"created_at_pr" gorm:"type:varchar(255)"`
+	ClosedAt        string  `json:"closed_at" gorm:"type:varchar(255)"`
+	Color           string  `json:"color" gorm:"type:varchar(255)"`
+	OwnerPR         *User   `gorm:"foreignKey:OwnerId"`
+	OwnerID         string  `json:"owner_id"`
+	Reviewers       []*User `gorm:"many2many:reviewers_pr;"`
+	Locked          bool    `json:"locked"`
+	CommitsUrl      string  `json:"commits_url" gorm:"type:varchar(255)"`
 	BranchName      string
 	IntroBranchName string
 }
