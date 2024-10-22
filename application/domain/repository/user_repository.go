1646da530b321ccf8d6e52734cd79af4a9316dcf
julio_filizzola <juliofilizzola@hotmail.com>
2024-10-22 13:34:55 -0300

    Rename userRepository to UserRepo
    Renamed the userRepository struct to UserRepo to follow Go naming conventions. Updated all associated method receivers and the constructor function accordingly.

--- application/domain/repository/user_repository.go
+++ application/domain/repository/user_repository.go
@@ -14,19 +14,19 @@ type UserRepository interface {
 	GetUserByGithubUsername(username string) (*model.User, error)
 }
 
-type userRepository struct {
+type UserRepo struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) UserRepository {
-	return &userRepository{db: db}
+func NewUserRepository(db *gorm.DB) *UserRepo {
+	return &UserRepo{db}
 }
 
-func (r *userRepository) CreateUser(user *model.User) error {
+func (r *UserRepo) CreateUser(user *model.User) error {
 	return r.db.Create(user).Error
 }
 
-func (r *userRepository) GetUserByID(id string) (*model.User, error) {
+func (r *UserRepo) GetUserByID(id string) (*model.User, error) {
 	var user model.User
 	if err := r.db.First(&user, id).Error; err != nil {
 		return nil, err
@@ -34,15 +34,15 @@ func (r *userRepository) GetUserByID(id string) (*model.User, error) {
 	return &user, nil
 }
 
-func (r *userRepository) UpdateUser(user *model.User) error {
+func (r *UserRepo) UpdateUser(user *model.User) error {
 	return r.db.Save(user).Error
 }
 
-func (r *userRepository) DeleteUser(id string) error {
+func (r *UserRepo) DeleteUser(id string) error {
 	return r.db.Delete(&model.User{}, id).Error
 }
 
-func (r *userRepository) GetUserByGithubUsername(username string) (*model.User, error) {
+func (r *UserRepo) GetUserByGithubUsername(username string) (*model.User, error) {
 	var user model.User
 	if err := r.db.Raw("SELECT * FROM users WHERE github_username = ?", username).Scan(&user).Error; err != nil {
 		return nil, fmt.Errorf("failed to find user by GitHub username %s: %w", username, err)
