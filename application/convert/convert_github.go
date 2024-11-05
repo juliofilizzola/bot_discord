8288f29598b6dba540278cb634b31aeca88c9946
Julio Filizzola <juliofilizzola@hotmail.com>
2024-11-05 13:09:16 -0300

    Add conversion function from Github to Discord format
    This commit introduces the `ConvertGithubToDiscord` function in `convert_github.go`. It takes a Github data structure and converts it to Discord's WebhookParams structure. This enhancement will facilitate seamless integration between Github events and Discord notifications.

--- application/convert/convert_github.go
+++ application/convert/convert_github.go
@@ -1,1 +1,11 @@
 package convert
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/juliofilizzola/bot_discord/application/domain"
+)
+
+func ConvertGithubToDiscord(data *domain.Github) discordgo.WebhookParams {
+
+	return discordgo.WebhookParams{}
+}
