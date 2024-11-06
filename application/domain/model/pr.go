be0020c87db6e1759efa443a64bc193f5f89c347
Julio Filizzola <juliofilizzola@hotmail.com>
2024-11-06 10:08:54 -0300

    Refactor PR model field definitions
    Reorder and inline the Base struct definition to follow struct field conventions. This enhances readability and maintains consistency in the code structure.

--- application/domain/model/pr.go
+++ application/domain/model/pr.go
@@ -1,8 +1,7 @@
 package model
 
 type PR struct {
-	ID              string `json:"id" valid:"uuid" gorm:"type:uuid;primary_key default:uuid_generate_v4()"`
-	Base            `valid:"required"`
+	ID              string  `json:"id" valid:"uuid" gorm:"type:uuid;primary_key default:uuid_generate_v4()"`
 	Url             string  `json:"url" gorm:"type:varchar(255)"`
 	Number          string  `json:"number" gorm:"type:varchar(255);uniqueIndex:number"`
 	State           string  `json:"state" gorm:"type:varchar(255)"`
@@ -12,11 +11,12 @@ type PR struct {
 	CreatedAtPr     string  `json:"created_at_pr" gorm:"type:varchar(255)"`
 	ClosedAt        string  `json:"closed_at" gorm:"type:varchar(255)"`
 	Color           int     `json:"color" gorm:"type:bigint"`
-	OwnerPR         *User   `json:"ownerPR" gorm:"foreignKey:OwnerId"`
 	OwnerID         string  `json:"owner_id"`
+	OwnerPR         *User   `json:"ownerPR" gorm:"foreignKey:OwnerId"`
 	Reviewers       []*User `json:"reviewers" gorm:"many2many:reviewers_pr;"`
 	Locked          bool    `json:"locked"`
 	CommitsUrl      string  `json:"commits_url" gorm:"type:varchar(255)"`
 	BranchName      string  `json:"branch_name" gorm:"type:varchar(255)"`
 	IntroBranchName string  `json:"intro_branch_name" gorm:"type:varchar(255)"`
+	Base            `valid:"required"`
 }
