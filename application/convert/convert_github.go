fde31834ab32067eaae21eca91817bf8fd522e41
julio_filizzola <juliofilizzola@hotmail.com>
2024-12-08 13:33:57 -0300

    Refactor: comment unused color constants.
    Commented out several color constants that are currently unused in the code to improve readability and maintainability. This change reduces clutter without impacting any existing functionality. These constants can be easily uncommented for future use if needed.

--- application/convert/convert_github.go
+++ application/convert/convert_github.go
@@ -14,14 +14,14 @@ import (
 )
 
 const (
-	ColorRed    = 15548997
-	ColorGreen  = 5763719
-	ColorBlue   = 3447003
-	ColorYellow = 16776960
-	ColorPurple = 10181046
+	ColorRed   = 15548997
+	ColorGreen = 5763719
+	//ColorBlue   = 3447003
+	//ColorYellow = 16776960
+	//ColorPurple = 10181046
 	ColorOrange = 15105570
 	ColorWhite  = 16777215
-	ColorBlack  = 0
+	//ColorBlack  = 0
 )
 
 const (
